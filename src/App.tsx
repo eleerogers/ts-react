c07080a6962ed6239efb0a709e02d2bf71008ff8
Lee Rogers <edwinleerogers@gmail.com>
2022-08-31 10:18:05 -0700

    basic functionality added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
+import { Todo } from './todo.model';
+
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([])
+  
+  const todoAddHandler = (text: string) => {
+    const newTodo = {id: Math.random().toString(), text}
+    setTodos(prevTodos => [...prevTodos, newTodo])
+  }
+
+  const deleteHandler = (id: string) => {
+    setTodos(prevTodos => prevTodos.filter(t => t.id !== id))
+  }
+
   return (
     <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList todos={todos} onDelete={deleteHandler} />
     </div>
   );
 }
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+interface TodoListProps {
+  todos: {id: string, text: string}[],
+  onDelete: (id: string) => void
+}
+
+const TodoList: React.FC<TodoListProps> = ({todos, onDelete}) => {
+  
+  return <ul>
+    {todos.map(todo => <li key={todo.id}>
+      <span>{todo.text}</span>
+      <button onClick={() => onDelete(todo.id)}>DELETE</button>
+    </li>)}
+  </ul>
+}
+
+export default TodoList;
